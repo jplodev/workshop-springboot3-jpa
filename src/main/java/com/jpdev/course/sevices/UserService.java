17950e34eda8e33647cd669017789c25dd7611bd
João Paulo <jplopapa.dev@gmail.com>
2025-01-22 18:19:50 -0300

    Exception handling - update

--- src/main/java/com/jpdev/course/sevices/UserService.java
+++ src/main/java/com/jpdev/course/sevices/UserService.java
@@ -4,6 +4,7 @@ import com.jpdev.course.entities.User;
 import com.jpdev.course.repositories.UserRepository;
 import com.jpdev.course.sevices.exceptions.DatabaseException;
 import com.jpdev.course.sevices.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -44,9 +45,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, obj);
-        return userRepository.save(entity);
+        try{
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
